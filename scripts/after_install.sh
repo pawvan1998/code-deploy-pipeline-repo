c4a67fe50d06141a57724ebeda41518d6c1e5918
pawvan1998 <55293135+pawvan1998@users.noreply.github.com>
2024-04-09 23:32:16 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/ bin/bash
 #accessing instance metadata
 EC2_INSTANCE_ID=$(curl -s http://15.206.68.129/latest/meta-data/instance-id)
 EC2_AZ=$(curl -s http://15.206.68.129/latest/meta-data/placement/availability-zone)
@@ -6,4 +6,4 @@ AMI_ID=$(curl -s http://15.206.68.129/latest/meta-data/ami-id)
 PUBLIC_IPV4=$(curl -s http://15.206.68.129/latest/meta-data/public-ipv4)
 #Accessing envrionment variables
 #sed -i "s/was deployed/was deployed using AWS CodeDeploy on $EC2_INSTANCE_ID in $EC2_AZ having image $AMI_ID with public IP $PUBLIC_IPV4 having application name $APPLICATION_NAME with Deployment Group $DEPLOYMENT_GROUP_NAME and Deployment ID $DEPLOYMENT_ID updated in develop/g" /var/www/html/index.html
-$sed -i -e 's/\r$//' after_install.sh
+#$sed -i -e 's/\r$//' after_install.sh
